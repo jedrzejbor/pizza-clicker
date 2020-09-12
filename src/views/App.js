64a043edc5b1e3da7dda6572389779d1b44f1466
jedrzejbor <jedrzej.borakiewicz@gmail.com>
2020-09-12 19:48:13 +0200

    Add structures file

--- src/views/App.js
+++ src/views/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { ReactComponent as BackgroundPizza } from "../images/background-pizza.svg";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BackgroundPizza />
+    </div>
+  );
 }
 
 export default App;
