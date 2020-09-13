c992e9a7904049f1b987816fefb20fcfc2d1c5e0
jedrzejbor <jedrzej.borakiewicz@gmail.com>
2020-09-13 18:34:43 +0200

    delete bgc

--- src/views/App.js
+++ src/views/App.js
@@ -1,12 +1,7 @@
 import React from "react";
-import { ReactComponent as BackgroundPizza } from "../images/background-pizza.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <BackgroundPizza />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
