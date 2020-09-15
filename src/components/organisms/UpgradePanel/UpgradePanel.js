b71da2ed6f565bd3faea013d1a6db5eba16aff12
jedrzejbor <jedrzej.borakiewicz@gmail.com>
2020-09-15 22:48:06 +0200

    Add upgradePanel

---
+++ src/components/organisms/UpgradePanel/UpgradePanel.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div``;
+
+const UpgradePanel = () => {
+  return <Wrapper></Wrapper>;
+};
+
+export default UpgradePanel;
--- src/views/App.js
+++ src/views/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import GlobalStyle from "../theme/GlobalStyle";
 import GamePanel from "../components/organisms/GamePanel/GamePanel";
+import UpgradePanel from "../components/organisms/UpgradePanel/UpgradePanel";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <GamePanel />
+      <UpgradePanel />
     </div>
   );
 }
