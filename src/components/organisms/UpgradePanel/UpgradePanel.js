d3d9fcddae078e9dd9642af46ed03849abe463af
jedrzejbor <jedrzej.borakiewicz@gmail.com>
2020-09-21 21:54:30 +0200

    clear upgrade panel

--- src/components/organisms/UpgradePanel/UpgradePanel.js
+++ src/components/organisms/UpgradePanel/UpgradePanel.js
@@ -3,72 +3,6 @@ import styled from "styled-components";
 import Button from "../../atoms/button/Button";
 import Title from "../../atoms/title/Title";
 
-// const optionsSec = [
-//   {
-//     id: 0,
-//     number: "01.",
-//     title: "Employee",
-//     money: 50,
-//   },
-//   {
-//     id: 1,
-//     number: "02.",
-//     title: "Advertisment",
-//     money: 500,
-//   },
-//   {
-//     id: 2,
-//     number: "03.",
-//     title: "Scooter",
-//     money: 5000,
-//   },
-//   {
-//     id: 3,
-//     number: "04.",
-//     title: "Car",
-//     money: 50000,
-//   },
-//   {
-//     id: 4,
-//     number: "05.",
-//     title: "Premises",
-//     money: 500000,
-//   },
-// ];
-// const optionsSec = { money: 50, title: "egurola" };
-const optionsClick = [
-  {
-    id: 0,
-    number: "01.",
-    title: "Margharita",
-    money: 50,
-  },
-  {
-    id: 1,
-    number: "02.",
-    title: "Funghi",
-    money: 500,
-  },
-  {
-    id: 2,
-    number: "03.",
-    title: "Capriciosa",
-    money: 5000,
-  },
-  {
-    id: 3,
-    number: "04.",
-    title: "Hawaii",
-    money: 50000,
-  },
-  {
-    id: 4,
-    number: "05.",
-    title: "Chicken",
-    money: 500000,
-  },
-];
-
 const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
@@ -160,7 +94,6 @@ const UpgradePanel = () => {
 
   const moreMoney = (index) => () => {
     console.log(index);
-    // console.log(e);
     let newArray = [...valueChange];
     newArray[index].money = Math.round(
       newArray[index].money + (newArray[index].money / 100) * 5
@@ -170,7 +103,6 @@ const UpgradePanel = () => {
 
   const moreMoneyClick = (index) => () => {
     console.log(index);
-    // console.log(e);
     let newArray = [...valueChangeClick];
     newArray[index].money = Math.round(
       newArray[index].money + (newArray[index].money / 100) * 5
